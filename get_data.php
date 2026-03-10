47b8423d8ef68a0bd4ef9846af1b3cc695a55442
ayaaaaaaaaaaaa11111 <alyanurjannah08@gmail.com>
2026-03-10 17:38:14 +0800

    Add files via upload

--- get_data.php
+++ get_data.php
@@ -1,19 +1,19 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_sentimen";
+header('Content-Type: application/json');
+$conn = mysqli_connect("localhost", "root", "", "db_sentimen");
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) {
+    echo json_encode(["error" => "Koneksi Gagal"]);
+    exit;
+}
 
-$sql = "SELECT * FROM hasil_sentimen";
-$result = mysqli_query($conn, $sql);
+$query = "SELECT * FROM hasil_sentimen";
+$result = mysqli_query($conn, $query);
 
-$data = array();
-while($row = mysqli_fetch_assoc($result)) {
+$data = [];
+while ($row = mysqli_fetch_assoc($result)) {
     $data[] = $row;
 }
 
-// Kirim data dalam format JSON agar bisa dibaca JavaScript
 echo json_encode($data);
 ?>
